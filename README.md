c3e97922032ea1ece4748aea720e167a43a33163
Edu Lazaro <edu@kenodo.com>
2024-04-04 14:44:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tutorial de Docker
+
+En este tutorial aprenderás a user tanto Docker como Docker Compose desce cero.
+
+
+### [Tutorial](https://www.neoguias.com/tutorial-docker/)
+
+## Autor
+
+- [Edu Lázaro](https://edulazaro.com)
+
+## License
+
+Este proyecto, no así el tutorial, es open source y está disponible bajo la **licencia MIT**.
---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const Plato = mongoose.model('Plato', new mongoose.Schema({
+  tipo: String,
+  estado: String,
+}))
+
+const app = express()
+
+mongoose.connect('mongodb://edu:testpass@imongo:27017/restaurante?authSource=admin')
+
+app.get('/platos', async (_req, res) => {
+  console.log('Mostrando lista de platos...')
+  const platos = await Plato.find();
+  return res.send(platos)
+})
+
+
+app.post('/platos', async (req, res) => {
+  try {
+    console.log('Creando plato...');
+    const { nombre, tipo } = req.body;
+
+    if (!nombre || !tipo) {
+      return res.status(400).send('Los campos nombre y tipo son obligatorios');
+    }
+
+    await Plato.create({ nombre, tipo }); // Utiliza las variables extraídas para crear el plato
+    return res.send('Plato creado con éxito');
+  } catch (error) {
+    console.error('Error creando el plato:', error);
+    return res.status(500).send('Ocurrió un error al crear el plato');
+  }
+});
+
+app.listen(3000, () => console.log('listening...'))
